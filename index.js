90ec0e440be5d5389ebc7b325263d3aaf193faf6
TashaRolland92 <tasharolland@hotmail.co.uk>
2025-05-15 12:31:14 +0100

    removed dotenv includes

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import dotenv from 'dotenv';
 import cors from "cors";
 import authRoutes from './routes/authRoutes.js';
 import accountRoutes from './routes/accountRoutes.js';
@@ -7,7 +6,6 @@ import categoriesRoutes from './routes/categories.js';
 import subcategoriesRoutes from './routes/subcategories.js';
 import treatmentRoutes from './routes/treatments.js';
 
-dotenv.config();
 const app = express();
 const port = 3000;
 
