6b0957e57205ca1af05f465f2219a2ff8bae1dae
TashaRolland92 <tasharolland@hotmail.co.uk>
2025-05-15 12:30:46 +0100

    moved dotenv into main db file

--- db.js
+++ db.js
@@ -1,7 +1,11 @@
 import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const db = await mysql.createConnection({
     host: process.env.DB_HOST,      // Or your MySQL host
+	port: process.env.DB_PORT,
     user: process.env.DB_USER,           // Your MySQL username
     password: process.env.DB_PASSWORD,  // Your MySQL password
     database: process.env.DB_NAME // Your DB name
