046366d7df0ef1f3f9f59e91cd62e573dccf33fc
TashaRolland92 <tasharolland@hotmail.co.uk>
2025-05-19 11:39:23 +0100

    manipulated subcategories to allow query params

--- routes/subcategories.js
+++ routes/subcategories.js
@@ -3,10 +3,20 @@ import db from '../db.js';
 
 const router = express.Router();
 
-// Get all subcategories
+// Get all subcategories or filter by categoryId
 router.get("/", async (req, res) => {
 	try {
-		const [subcategories] = await db.query("SELECT * FROM treatment_subcategories");
+		const categoryId = req.query.categoryId;
+
+		let query = "SELECT * FROM treatment_subcategories";
+		let params = [];
+
+		if (categoryId) {
+			query += " WHERE category_id = ?";
+			params.push(categoryId);
+		}
+
+		const [subcategories] = await db.query(query, params);
 		res.json(subcategories);
 	} catch (err) {
 		console.error("Error fetching subcategories:", err);
